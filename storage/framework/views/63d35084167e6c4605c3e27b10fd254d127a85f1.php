2e2a3ef0005acc2e96b426c71e72502488ce79e8
aronoom <aronommeniaina@gmail.com>
2017-09-19 13:43:44 +0300

    After true .gitignore

--- storage/framework/views/63d35084167e6c4605c3e27b10fd254d127a85f1.php
+++
@@ -1,6 +0,0 @@
-<?php /* Html::style('materialize/css/materialize.min.css') */ ?>
-<?php echo e(Html::style('css/base.tutoriel.css')); ?>
-
-<?php echo e(Html::style('css/base.accueil.css')); ?>
-
-<?php echo e(Html::style('prismjs/css/prism-monokai.css')); ?>
